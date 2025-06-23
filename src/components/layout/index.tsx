884a9a922c286d40c183398923239e0dcf1ccb96
@k_aldi <aldikurniawan1606@gmail.com>
2025-06-23 23:56:55 +0700

    add react hook form login

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -1,14 +1,18 @@
 "use client";
 
 import StoreProvider from "@/provider/storeProvider";
+import { ThemeProvider } from "@mui/material";
 import React from "react";
+import theme from "../theme";
 
 const HiFiLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <StoreProvider>
-      <main className="h-full min-h-screen w-full background-radial">
-        {children}
-      </main>
+      <ThemeProvider theme={theme}>
+        <main className="h-full min-h-screen w-full background-radial">
+          {children}
+        </main>
+      </ThemeProvider>
     </StoreProvider>
   );
 };
---
+++ src/modules/login/index.tsx
@@ -0,0 +1,158 @@
+"use client";
+
+import React from "react";
+import { useForm } from "react-hook-form";
+import { loginAdmin } from "@/features/auth/authSlice";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+import { useAppDispatch, useAppSelector } from "@/store/hooks";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import FormLabel from "@mui/material/FormLabel";
+import FormControl from "@mui/material/FormControl";
+import TextField from "@mui/material/TextField";
+import Typography from "@mui/material/Typography";
+import MuiCard from "@mui/material/Card";
+import { styled } from "@mui/material/styles";
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
+
+type LoginFormData = {
+  email: string;
+  password: string;
+};
+
+const Card = styled(MuiCard)(({ theme }) => ({
+  display: "flex",
+  flexDirection: "column",
+  alignSelf: "center",
+  width: "100%",
+  padding: theme.spacing(4),
+  gap: theme.spacing(2),
+  margin: "auto",
+  [theme.breakpoints.up("sm")]: {
+    maxWidth: "450px",
+  },
+  boxShadow:
+    "hsla(220, 30%, 5%, 0.05) 0px 5px 15px 0px, hsla(220, 25%, 10%, 0.05) 0px 15px 35px -5px",
+  ...theme.applyStyles("dark", {
+    boxShadow:
+      "hsla(220, 30%, 5%, 0.5) 0px 5px 15px 0px, hsla(220, 25%, 10%, 0.08) 0px 15px 35px -5px",
+  }),
+}));
+
+export default function LoginModule() {
+  const dispatch = useAppDispatch();
+  const router = useRouter();
+  const { token } = useAppSelector((state) => state.auth);
+
+  const schema = yup.object({
+    email: yup
+      .string()
+      .email("invalid email!")
+      .required("Email must be filled!"),
+    password: yup.string().required("Password must be filled!"),
+  });
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isDirty },
+    watch,
+  } = useForm<LoginFormData>({
+    mode: "all",
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+    resolver: yupResolver(schema),
+  });
+  console.log(watch(), errors);
+
+  const onSubmit = (data: LoginFormData) => {
+    dispatch(loginAdmin(data));
+  };
+
+  useEffect(() => {
+    if (token) router.push("/employee");
+  }, [token, router]);
+
+  return (
+    <div className="flex justify-center items-center h-screen w-full">
+      <Card variant="outlined">
+        <Typography
+          component="h1"
+          variant="h4"
+          sx={{ width: "100%", fontSize: "clamp(2rem, 10vw, 2.15rem)" }}
+        >
+          Sign in
+        </Typography>
+        <Box
+          component="form"
+          noValidate
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            width: "100%",
+            gap: 2,
+          }}
+        >
+          <FormControl>
+            <FormLabel htmlFor="email">Email</FormLabel>
+            <TextField
+              {...register("email", {
+                required: "Email wajib diisi",
+                pattern: {
+                  value: /^\S+@\S+$/i,
+                  message: "Format email tidak valid",
+                },
+              })}
+              error={errors?.email?.message !== ""}
+              helperText={errors?.email?.message}
+              id="email"
+              type="email"
+              name="email"
+              placeholder="your@email.com"
+              autoComplete="email"
+              autoFocus
+              required
+              fullWidth
+              variant="outlined"
+            />
+          </FormControl>
+          <FormControl>
+            <FormLabel htmlFor="password">Password</FormLabel>
+            <TextField
+              {...register("password", {
+                required: "Password wajib diisi",
+                minLength: {
+                  value: 6,
+                  message: "Minimal 6 karakter",
+                },
+              })}
+              error={errors?.password?.message !== ""}
+              helperText={errors?.password?.message}
+              name="password"
+              placeholder="••••••"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+              autoFocus
+              required
+              fullWidth
+              variant="outlined"
+            />
+          </FormControl>
+          <Button
+            disabled={!isDirty}
+            onClick={handleSubmit(onSubmit)}
+            fullWidth
+            variant="contained"
+          >
+            Sign in
+          </Button>
+        </Box>
+      </Card>
+    </div>
+  );
+}
